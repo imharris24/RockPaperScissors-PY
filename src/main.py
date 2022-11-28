0ce3b70165e7fa138eecd835828338e064322f0f
Muhammad Harris <harris20014@gmail.com>
2022-11-28 18:11:30 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,30 @@
 import random
 
+choice = ['r', 'p', 's']
 # function to input player choice and return its value
 def getPlayerChoice():
     playerChoice = input("""Choose Your Move
     - 'r' for Rock
     - 'p' for Paper
     - 's' for Scissors
-    Input: """)
+    Input: """)[0]
+    playerChoice = playerChoice.lower()
+    while playerChoice not in choice:
+        playerChoice = input("""Error: INVALID INPUT
+        Input again: """)
+        playerChoice = playerChoice.lower()
     return playerChoice
 # function to generate computer choice and return its value
 def getComputerChoice():
-    computerChoice = random.choice(['r', 'p', 's'])
+    computerChoice = random.choice(choice)
     return computerChoice
-
+# function that returns result
+def returnResult(player, computer):
+    if player == computer:
+        return [0, 0]
+    elif (player == 'r' and computer == 's') or (player == 'p' and computer == 'r') or (player == 's' and computer == 'p'):
+        return [1, 0]
+    else:
+        return [0, 1]
+    
 # main
